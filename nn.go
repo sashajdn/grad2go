4b48630c1a4dea82ea1d527b70ab34c7499fa0a1
sasha <alexperkins.dev@gmail.com>
2023-04-05 14:09:44 +0900

    add(neuron): add neuron

--- nn.go
+++ nn.go
@@ -1,3 +1,80 @@
 package grad2go
 
+import (
+	"log"
+	"math/rand"
+	"time"
+
+	"github.com/shopspring/decimal"
+)
+
+var minusOne = decimal.NewFromFloat(-1.0)
+
 type NeuralNetwork struct{}
+
+func NewNeuron(numberOfInputs int) *Neuron {
+	n := &Neuron{
+		r: rand.New(rand.NewSource(time.Now().UnixNano())),
+		d: numberOfInputs,
+	}
+
+	n.W = n.generateRandomVector(numberOfInputs, 1)
+	n.B = n.generateRandomVector(1, 1)
+
+	return n
+}
+
+type Neuron struct {
+	W []*Value
+	B []*Value
+	r *rand.Rand
+	d int
+}
+
+func (n *Neuron) Forward(inputs []*Value) *Value {
+	if len(inputs) != n.d {
+		log.Fatalf("invalid dim of inputs: got %d, expected %d", len(inputs), n.d)
+	}
+
+	// w * x + b
+	var sum = n.B[0]
+	for i := 0; i < n.d; i++ {
+		w := n.W[i]
+		x := inputs[i]
+		product := w.Mul(x)
+
+		sum = sum.Add(product)
+	}
+	activation := sum.ReLu()
+
+	return activation
+}
+
+func (n *Neuron) generateRandomVector(size int, linSpace float64) []*Value {
+	switch {
+	case linSpace == 0:
+		linSpace = 1
+	case linSpace < 0:
+		linSpace = -1.0 * linSpace
+	}
+
+	var out = make([]*Value, size)
+
+	for i := 0; i < size; i++ {
+		negative := n.r.Float64() > 0.5
+		value := decimal.NewFromFloat(n.r.Float64())
+
+		if linSpace != 1 {
+			rangeMul := decimal.NewFromFloat(linSpace)
+			value = value.Mul(rangeMul)
+		}
+
+		if negative {
+			value = value.Mul(minusOne)
+		}
+
+		out[i] = NewValue(value, OperationNOOP)
+	}
+
+	return out
+}
